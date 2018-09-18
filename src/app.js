bc1a1fd0186111a99b4fb78e2527d57a39b03fba
Tyler Arnold <tyler@tyler.macpractice.lan>
2018-09-18 08:32:01 -0500

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import configureStore from './store/configureStore';
+
+const jsx = (
+    <div>
+        <h1>This is my main page</h1>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }
+    ]
+    },
+    devServer: {
+        publicPath: "/",
+        contentBase: path.join(__dirname, 'public',),
+        historyApiFallback: true
+    }
+};
